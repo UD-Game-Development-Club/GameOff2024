aec9a8eeff8ca47d83cbcf76372e5f735ed68a78
Quang Nguyen <quang.nguyen5049@gmail.com>
2024-11-18 00:08:11 -0500

    Update Interact Interface and Split DialogueTrigger

--- game/Assets/Scripts/DialogueTrigger.cs
+++ game/Assets/Scripts/DialogueTrigger.cs
@@ -1,20 +1,15 @@
 using UnityEngine;
 
-public class DialogueTrigger : MonoBehaviour, IInteractable
+public class DialogueTrigger : MonoBehaviour
 {
     [Header("Ink JSON")]
     [SerializeField] private TextAsset inkJSON;
-
-    public void Interact()
-    {
-        DialogueManager.Instance.StartDialogue(inkJSON);
-    }
 
     public void OnTriggerEnter(Collider collider) 
     {
         if (collider.gameObject.tag == "Player")
         {
-            Interact();
+             DialogueManager.Instance.StartDialogue(inkJSON);
         }
     }
 }
