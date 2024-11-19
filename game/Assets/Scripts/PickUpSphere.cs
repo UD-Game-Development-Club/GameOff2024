af5b36ec321373cc69b700f628139c752c3e33d9
Daniel Frederick <frederickd5@udayton.edu>
2024-11-18 21:02:49 -0500

    small adjustment

--- game/Assets/Scripts/PickUpSphere.cs
+++ game/Assets/Scripts/PickUpSphere.cs
@@ -6,12 +6,7 @@ public class PickUpSphere : MonoBehaviour, IInteractable
 
     private void Start()
     {
-        // Find the PickUpController in the scene (usually on the player/camera)
         pickUpController = Camera.main.GetComponent<PickUpController>();
-        if (pickUpController == null)
-        {
-            Debug.LogError("PickUpController not found on main camera!");
-        }
     }
 
     public void Interact()
