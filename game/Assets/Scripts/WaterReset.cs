ea8db0faa039544d797759a3a5012ccf45dde991
Ryan Zmuda <ryanzmuda@gmail.com>
2024-11-30 21:35:13 -0500

    im losing my mind

--- game/Assets/Scripts/WaterReset.cs
+++ game/Assets/Scripts/WaterReset.cs
@@ -5,9 +5,15 @@ public class WaterReset : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        //Debug.Log("somting entered water");
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            //Debug.Log("Player entered water");
+            //other.transform.position = new Vector3(-257.02f, 0f, -217.6f);
+
+            // get object named "Sapling Present" and move the player to it
+            GameObject saplingPresent = GameObject.Find("Tiny Rock 3");
+            other.transform.position = saplingPresent.transform.position;
         }
     }
 }
